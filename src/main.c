3ec776f07fcac3d8f4bbeb1d93adbea9d81c2e0b
José Roseiro <zrz@Joses-MacBook-Pro-5.local>
2024-09-06 19:34:34 +0200

    pseudo code done and basic structure

--- src/main.c
+++ src/main.c
@@ -3,37 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jroseiro <jroseiro@student.42.fr>          +#+  +:+       +#+        */
+/*   By: zrz <zrz@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/30 18:42:00 by jroseiro          #+#    #+#             */
-/*   Updated: 2024/08/30 19:09:26 by jroseiro         ###   ########.fr       */
+/*   Updated: 2024/09/06 19:34:02 by zrz              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/include.h"
+
+				/*				PSEUDO CODE				*/
+
+//Declare pointers to two linked lists, one for stack A and one for stack B
+		//Set both pointers to NULL to avoid undefined behaviour and indicate we're starting with empty stacks
+
+//Handle input count errors. AC must be 2 or more, and the second input must not be empty
+		//If input errors, return error
+
+//Handle both cases of input, whether a variable number of command line arguments, or as a string
+		//If the input of numbers comes as a string, call ft_split() to split the substrings
+
+//Initialize stack A by appending each input number as a node to stack A
+		//Handle integer overflow, duplicates and syntax errors, ex: input must contain only digits or - or + signs
+				// if errors found, free stack A and return error
+		//Check each input, if it is a long integer
+				// if input is string, convert it to a long integer (long is used to handle larger numbers)
+		//(if all else dealt with) Append the nodes to stack A
+
+//Check if stack A is sorted
+		//If not sorted, implement our sorting algorithm
+				//check if stack A contains only 2 numbers
+						//if its just 2 numbers, simply swap them
+				//check if stack A contains only 3 numbers
+						//if so, implement the simple 'sort three' algorithm
+				//check if stack A has more than 3 numbers
+						//if so, implement the türk
+//Clean up the stack
 
 
-
-// 2 stacks, 'a' and 'b'
-// at start:
-	// a contains a random amount of negative and/or positive numbers which cannot be duplicated
-	// the stack b is empty
-// goal is to sort in ascending order numbers into stack *a*
-
-/*to do so, we can:
-
-	sa -> (swap a) : Swap the first 2 elements at the top of stack ' a '
-					Do nothing if there is only one or no elements
-
-	sb -> (swap b) ; Swap the first 2 elements at the top of stack ' b '
-					Do nothing if there is only one or no elements
-
-	ss -> ' sa ' and ' sb ' at the same time
-
-	pa -> push a
-
-
-
-
-
-
+/*	BASIC UTILS 
+	functions for:
+	- Error handling
+	- Doing the Operations
+	- Measuring stack length
+	- Finding the last node
+	- Finding the Min and Max nodes
 */
